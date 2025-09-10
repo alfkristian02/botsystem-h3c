16003918d3926ee1474e94795a1d76f2144a4284
alfkristian02 <alfkristian02@gmail.com>
2025-09-10 23:02:55 +0200

    feat: set up firebase and easy (temp) read to test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,34 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+  import { initializeApp } from "firebase/app";
+  import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
+  import { onMount } from "svelte";
+
+  const firebaseConfig = {
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket:import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGE_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  };
+
+  const app = initializeApp(firebaseConfig);
+  const db = getFirestore(app);
+
+  let regler = [];
+
+  async function getRules(db) {
+    const reglerCol = collection(db, "prikk_regler");
+    const reglerSnapshot = await getDocs(reglerCol);
+    const reglerList = reglerSnapshot.docs.map((doc) => doc.data());
+    return reglerList;
+  }
+
+  onMount(async () => { 
+    regler = await getRules(db);
+  });
+</script>
+
+{#each regler as item}
+  <li>{JSON.stringify(item)}</li>
+{/each}
