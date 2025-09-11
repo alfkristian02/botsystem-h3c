c0b0b80f5048cfef448fbc77dd0614630330cf53
alfkristian02 <alfkristian02@gmail.com>
2025-09-11 22:39:59 +0200

    feat: dialogbox to show rules and fines

---
+++ src/lib/components/DialogBox.svelte
@@ -0,0 +1,104 @@
+<script lang="ts">
+  import { onMount, tick } from "svelte";
+  import { database } from "$lib/utils/firestore";
+  import { type Firestore, collection, getDocs } from "firebase/firestore/lite";
+
+  let { toggleShowDialog } = $props();
+
+  let regler: Array<Rule> = $state([]);
+
+  type Rule = {
+    description: string;
+    fine: number;
+  };
+
+  async function getRules(db: Firestore) {
+    const reglerCol = collection(db, "prikk_regler");
+    const reglerSnapshot = await getDocs(reglerCol);
+    const reglerList: Array<Rule> = reglerSnapshot.docs.map((doc) => {
+      let data = doc.data();
+      return {
+        description: data.regel,
+        fine: data.bot,
+      };
+    });
+    return reglerList;
+  }
+
+  onMount(async () => {
+    regler = await getRules(database);
+  });
+</script>
+
+<div class="complete-overlay"></div>
+<div class="dialog-box">
+  <h2>Regler og straff for bøter</h2>
+  <br />
+  {#each regler as rule}
+    <span>
+      <p class="description">{rule.description}</p>
+      <p class="fine">{rule.fine}kr</p>
+    </span>
+  {/each}
+
+  <button class="dialog-button" onclick={toggleShowDialog}> Lukk </button>
+</div>
+
+<style>
+  .complete-overlay {
+    position: fixed;
+    background-color: black;
+
+    top: 0;
+    left: 0;
+
+    opacity: 0.3;
+
+    width: 100vw;
+    height: 100vh;
+  }
+
+  .dialog-box {
+    position: fixed;
+    background-color: white;
+    top: 50%;
+    left: 50%;
+
+    transform: translate(-50%, -50%);
+
+    border-radius: 10px;
+    padding: 30px;
+
+    max-height: 80vh;
+    overflow: scroll;
+
+    width: 70vw;
+    max-width: 850px;
+  }
+
+  h2 {
+    text-align: center;
+  }
+
+  span {
+    display: grid;
+    grid-template-columns: 1fr auto;
+    gap: 2rem;
+    align-items: center;
+  }
+
+  .fine {
+    align-self: center;
+  }
+
+  .dialog-button {
+    position: sticky;
+    bottom: 0;
+    float: right;
+
+    background-color: white;
+    border-radius: 15px;
+    padding: 10px;
+    font-size: 15px;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,34 +1,39 @@
-<script>
-  import { initializeApp } from "firebase/app";
-  import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
-  import { onMount } from "svelte";
+<script lang="ts">
+  import DialogBox from "$lib/components/DialogBox.svelte";
 
-  const firebaseConfig = {
-    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-    storageBucket:import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGE_SENDER_ID,
-    appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  };
+  let showDialog: boolean = $state(false);
 
-  const app = initializeApp(firebaseConfig);
-  const db = getFirestore(app);
-
-  let regler = [];
-
-  async function getRules(db) {
-    const reglerCol = collection(db, "prikk_regler");
-    const reglerSnapshot = await getDocs(reglerCol);
-    const reglerList = reglerSnapshot.docs.map((doc) => doc.data());
-    return reglerList;
+  const noe = function toggleDialogBox() {
+    showDialog = !showDialog;
   }
 
-  onMount(async () => { 
-    regler = await getRules(db);
-  });
 </script>
 
-{#each regler as item}
-  <li>{JSON.stringify(item)}</li>
-{/each}
+<button
+  onclick={noe}
+  class="open-rules">?</button
+>
+
+{#if showDialog}
+  <DialogBox toggleShowDialog={noe} />
+{/if}
+
+<style>
+  .open-rules {
+    border-radius: 100%;
+    padding: 10px;
+    text-align: center;
+    position: absolute;
+    right: 2rem;
+    top: 2rem;
+    height: 3rem;
+    width: 3rem;
+    background-color: white;
+  }
+
+  .open-rules:hover {
+    background-color: rgb(241, 241, 241);
+  }
+
+
+</style>
